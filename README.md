2423ea33519fea208c448c2a1fb9774695167818
zhengze <zhengze@sina.com.cn>
2020-04-08 21:46:38 +0800

    repo init

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # file_to_db
 load data from csv/xlsx  into database
+
+## INSTALL
+- pipenv install 
+or 
+- pip install -r requirements.txt
+
+## CONFIG
+- $sudo touch .env 
+```
+DATABASE_URI=postgresql+psycopg2://admin:1234@localhost/test
+```
+
+## USAGE
+- python file_to_db.py -h
---
+++ file_to_db.py
@@ -0,0 +1,25 @@
+import os
+from sqlalchemy import create_engine
+import pandas as pd
+import argparse
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+arg = argparse.ArgumentParser()
+engine = create_engine(os.getenv('DATABASE_URI'))
+
+
+def import_file(arg):
+    df = pd.read_csv(arg.file, sep=',')
+    df.columns = [c.lower() for c in df.columns]
+    df.to_sql(arg.table, engine)
+
+
+if __name__ == '__main__':
+    arg.add_argument("--file", "-f", default="",
+                     type=str, help="the file path.")
+    arg.add_argument("--table", "-t", default="",
+                     type=str, help="the table name.")
+    args = arg.parse_args()
+    import_file(args)
